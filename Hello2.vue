da3fbd510fd0825abffca875352cf631e38fa0b8
ApinHuang <144195517+ApinHuang@users.noreply.github.com>
2023-09-07 16:17:35 +0800

    新增標題

--- Hello2.vue
+++ Hello2.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    
+    <h1>標題</h1> 
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
